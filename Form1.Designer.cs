8a3e01871a6e2b478a45e8911c1d14e8b77150bf
Vladislav Krestinin <vkrestinin@vsgate.io>
2020-09-06 11:45:52 +0300

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dlgOpenObjFile = new System.Windows.Forms.OpenFileDialog();
+            this.btnLoadObj = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dlgOpenObjFile
+            // 
+            this.dlgOpenObjFile.FileName = "openFileDialog1";
+            // 
+            // btnLoadObj
+            // 
+            this.btnLoadObj.Location = new System.Drawing.Point(641, 104);
+            this.btnLoadObj.Name = "btnLoadObj";
+            this.btnLoadObj.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadObj.TabIndex = 0;
+            this.btnLoadObj.Text = "Load obj";
+            this.btnLoadObj.UseVisualStyleBackColor = true;
+            this.btnLoadObj.Click += new System.EventHandler(this.btnLoadObj_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLoadObj);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog dlgOpenObjFile;
+        private System.Windows.Forms.Button btnLoadObj;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using ObjParser;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        private Obj _obj;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnLoadObj_Click(object sender, EventArgs e)
+        {
+            if (dlgOpenObjFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    _obj = new Obj();
+                    _obj.LoadObj(dlgOpenObjFile.FileName);
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+    }
+}
