273aacccb6b97626d95388b4cc128c43c62b3c24
Vladislav Krestinin <vkrestinin@vsgate.io>
2020-09-18 09:18:55 +0300

    add Constant project to git

---
+++ lab1.Core/Constant.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace lab1
+{
+    public static class Constant
+    {
+        public static readonly Vector3 SpawnPosition = new Vector3(1, 0, 0);
+        public const float InitYaw = 0f;
+        public const float InitPitch = 0f;
+    }
+}
---
+++ lab1.Core/Vector3Extensions.cs
@@ -0,0 +1,19 @@
+﻿namespace System.Numerics
+{
+    public static class Vector3Extensions
+    {
+        public static Matrix4x4 CreateRotation(this Vector3 vector, float radian)
+        {
+            var cos = (float)Math.Cos(radian);
+            var sin = (float)Math.Sin(radian);
+            var x = vector.X;
+            var y = vector.Y;
+            var z = vector.Z;
+            return new Matrix4x4(
+                    cos + (1 - cos) * x * x, (1 - cos) * x * y - sin * z, (1 - cos) * x * z + sin * y, 0,
+                    (1 - cos) * y * x + sin * z, cos + (1 - cos) * y * y, (1 - cos) * y * z - sin * x, 0,
+                    (1 - cos) * z * x - sin * y, (1 - cos) * z * y + sin * x, cos + (1 - cos) * z * z, 0,
+                    0, 0, 0, 1);
+        }
+    }
+}
